aacaa070468a1db596e4a780ff9a62c14bdb6d69
Sasha <alexh02@mail.ru>
2022-12-20 17:19:08 +0300

    v.0

--- main.cpp
+++ main.cpp
@@ -3,5 +3,35 @@ using namespace std;
 int main()
 {
 	setlocale (LC_ALL, "Rus");
+	double a,b,c;
+	char n;
+	cout << "Первое число" << endl; cin >> a;
+	cout << "Второе число" << endl; cin >> b;
+	cout << "выберите выражение" << endl; cin >> n;
+	
+	
+	
+	
+	
+	switch (n)
+    {
+        case '1':   c=a+b;
+                    break;
+ 
+        case '2':   c=a-b;
+                    break;
+ 
+        case '3':   c=a*b;
+                    break;
+ 
+        case '4':   c=a/b;
+                    break;
+ 
+        default:    cout <<"Не верное значение."; 
+    }
+
+	cout << "Результат :" << c << endl;
+
+
 	system("pause");
 }
