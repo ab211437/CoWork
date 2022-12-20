f1902b92c97d2bf812d405b03a6dc3b63ba38417
Sasha <alexh02@mail.ru>
2022-12-20 20:14:42 +0300

    Подготовка к последнему слиянию

--- main.cpp
+++ main.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
+#include "func.h"
 using namespace std;
 int main()
 {
-	setlocale (LC_ALL, "Rus");
-	double a,b,c;
+	setlocale(LC_ALL, "Rus");
+	double a, b;
 	char n;
-	cout << "Первое число" << endl; cin >> a;
-	cout << "Второе число" << endl; cin >> b;
-	cout << "выберите выражение" << endl; cin >> n;
-	
-	
-	
-	
-	
+    cout << "Введите первое число" << endl; cin >> a;
+    cout << "Введите второе число" << endl; cin >> b;
+    cout << "Введите выражениие" << endl; cin >> n;
 	switch (n)
-    {
-        case '1':   c=a+b;
-                    break;
- 
-        case '2':   c=a-b;
-                    break;
- 
-        case '3':   c=a*b;
-                    break;
- 
-        case '4':   c=a/b;
-                    break;
- 
-        default:    cout <<"Не верное значение."; 
-    }
+	{
+	case '+':
+        cout << "Ответ: " << Sum(a, b) << endl;
+		break;
+	case '-':
+        cout << "Ответ: " << Vih(a, b) << endl;
+		break;
+	case '*':
+        cout << "Ответ: " << Umn(a, b) << endl;
+		break;
+	case '/':
+        cout << "Ответ: " << Del(a, b) << endl;
+		break;
 
-	cout << "Результат :" << c << endl;
-
-
+    default: cout << "Ошибка" << endl;
+	}
 	system("pause");
+	return 0;
+	
 }
