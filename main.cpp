0b8f9a5f4d83a551d4248721c34b35a95a432058
NikitaVarov <nikitos.varov@yandex.ru>
2022-12-20 17:09:43 +0300

    add switch

--- main.cpp
+++ main.cpp
@@ -3,5 +3,28 @@ using namespace std;
 int main()
 {
 	setlocale (LC_ALL, "Rus");
+	
+
+
+	switch (n)
+    {
+        case '1':   c=a+b;
+                    break;
+ 
+        case '2':   c=a-b;
+                    break;
+ 
+        case '3':   c=a*b;
+                    break;
+ 
+        case '4':   c=a/b;
+                    break;
+ 
+        default:    cout <<"Не верное значение."; 
+    }
+
+
+
+
 	system("pause");
 }
